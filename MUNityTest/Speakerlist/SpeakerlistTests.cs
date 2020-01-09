ddf660f3eee09bf840cc0326e4f78c4d324ed998
Aeuke Peer Conradi <aeuke-peer@t-online.de>
2020-01-09 16:16:21 +0100

    Removed Speakerlist Tests, to be redone

--- MUNityTest/Speakerlist/SpeakerlistTests.cs
+++ MUNityTest/Speakerlist/SpeakerlistTests.cs
@@ -16,32 +16,5 @@ namespace MUNityTest.Speakerlist
             Assert.IsNotNull(instance);
         }
 
-        [Test]
-        public void TestAddSpeaker()
-        {
-            var instance = new SpeakerlistModel();
-            instance.AddSpeaker(DelegationHandler.AllDefaultDelegations()[0]);
-            Assert.AreEqual(1, instance.Speakers.Count);
-            Assert.AreEqual(DelegationHandler.AllDefaultDelegations()[0], instance.Speakers[0]);
-        }
-
-        [Test]
-        public void TestAddQuestion()
-        {
-            var instance = new SpeakerlistModel();
-            instance.AddQuestion(DelegationHandler.AllDefaultDelegations()[0]);
-            Assert.AreEqual(1, instance.Questions.Count);
-            Assert.AreEqual(DelegationHandler.AllDefaultDelegations()[0], instance.Questions[0]);
-        }
-
-        [Test]
-        public void TestRemoveSpeaker()
-        {
-            var instance = new SpeakerlistModel();
-            instance.AddSpeaker(DelegationHandler.AllDefaultDelegations()[0]);
-            instance.RemoveSpeaker(DelegationHandler.AllDefaultDelegations()[0]);
-            Assert.AreEqual(0, instance.Speakers.Count);
-        }
-
     }
 }
