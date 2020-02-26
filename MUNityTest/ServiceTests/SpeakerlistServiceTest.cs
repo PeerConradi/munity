a9ad096b98b4119380ce3289802a6c6ce6433d7e
PeerConradi <peerconradi@gmail.com>
2020-02-26 17:18:48 +0100

    Added StartStopTest

--- MUNityTest/ServiceTests/SpeakerlistServiceTest.cs
+++ MUNityTest/ServiceTests/SpeakerlistServiceTest.cs
@@ -21,7 +21,25 @@ namespace MUNityTest.ServiceTests
             speakerlist.NextSpeaker();
             speakerlist.StartSpeaker();
             System.Threading.Thread.Sleep(5000);
-            Assert.IsTrue(speakerlist.RemainingSpeakerTime < new TimeSpan(0, 1, 0));
+            Assert.IsTrue(speakerlist.RemainingSpeakerTime < new TimeSpan(0, 0, 57));
+            Assert.IsTrue(speakerlist.RemainingSpeakerTime > new TimeSpan(0, 0, 50));
+        }
+
+        [Test]
+        public void StartStopShouldContinueTest()
+        {
+            var service = new SpeakerlistService();
+            var speakerlist = service.CreateSpeakerlist();
+            speakerlist.Speakertime = new TimeSpan(0, 1, 0);
+            var delegation = new DelegationModel();
+            delegation.Name = "Test";
+            speakerlist.AddSpeaker(delegation);
+            speakerlist.NextSpeaker();
+            speakerlist.StartSpeaker();
+            System.Threading.Thread.Sleep(5000);
+            speakerlist.PauseSpeaker();
+            speakerlist.StartSpeaker();
+            Assert.IsTrue(speakerlist.RemainingSpeakerTime < new TimeSpan(0, 0, 57));
             Assert.IsTrue(speakerlist.RemainingSpeakerTime > new TimeSpan(0, 0, 50));
         }
     }
