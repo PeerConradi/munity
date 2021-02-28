e0bfa0674ef5c2ff7dcabb9b8491b7ad79c5ab1a
Aeuke Peer Conradi <peerconradi@gmail.com>
2021-02-28 23:58:13 +0100

    minor changes

---
+++ src/MUNityCore/Controllers/Resa/OperativeParagraphController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MUNityCore.Controllers.Resa
+{
+    [Route("api/Resa/Operative[controller]")]
+    [ApiController]
+    public class OperativeParagraphController : ControllerBase
+    {
+    }
+}
