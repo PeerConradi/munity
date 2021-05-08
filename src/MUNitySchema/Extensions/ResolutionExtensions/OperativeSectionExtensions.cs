a75182cfe52012139c3d2543d4030983aebea18a
Aeuke Peer Conradi <peerconradi@gmail.com>
2021-05-08 23:24:23 +0200

    Minor bugfixes in Resa Editor

--- src/MUNitySchema/Extensions/ResolutionExtensions/OperativeSectionExtensions.cs
+++ src/MUNitySchema/Extensions/ResolutionExtensions/OperativeSectionExtensions.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace MUNity.Extensions.ResolutionExtensions
 {
@@ -10,6 +11,34 @@ namespace MUNity.Extensions.ResolutionExtensions
         public static int AmendmentCount(this OperativeSection section)
         {
             return section.AddAmendments.Count + section.ChangeAmendments.Count + section.DeleteAmendments.Count + section.MoveAmendments.Count;
+        }
+
+        /// <summary>
+        /// Only works on one level
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="paragraph"></param>
+        /// <returns></returns>
+        public static int GetIndexOfParagraphNonVirtual(this OperativeSection section, OperativeParagraph paragraph)
+        {
+            int index = 0;
+            foreach(var p in section.Paragraphs)
+            {
+                if (!p.IsVirtual)
+                {
+                    if (p == paragraph ||p.OperativeParagraphId == paragraph.OperativeParagraphId)
+                    {
+                        break;
+                    }
+                    index++;
+                }
+            }
+            return index;
+        }
+
+        public static int GetIndexOfParagraph(this OperativeSection section, OperativeParagraph paragraph)
+        {
+            return section.Paragraphs.IndexOf(paragraph);
         }
 
         public static IEnumerable<AbstractAmendment> AmendmentsOrdered (this OperativeSection section)
