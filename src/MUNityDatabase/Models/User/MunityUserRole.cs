42b6f09809d69f00234f0e0c3a38c7b1c330536a
Aeuke Peer Conradi <aeuke-peer@t-online.de>
2021-11-13 08:59:47 +0100

    Fixed small bug in EF Model

--- src/MUNityDatabase/Models/User/MunityUserRole.cs
+++ src/MUNityDatabase/Models/User/MunityUserRole.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,10 @@ namespace MUNity.Database.Models.User;
 
 public class MunityUserRole : IdentityUserRole<string>
 {
+    [ForeignKey(nameof(UserId))]
     public virtual MunityUser User { get; set; }
 
+    [ForeignKey(nameof(RoleId))]
     public virtual MunityRole Role { get; set; }
 
     public MunityUserRole()
