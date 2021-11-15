2c6079873d0c659264d8395accd42f1e2fee98a5
Aeuke Peer Conradi <aeuke-peer@t-online.de>
2021-11-15 23:17:15 +0100

    Added coverage report

--- generatecoverage.sh
+++ generatecoverage.sh
@@ -1,4 +1,4 @@
-rm -r ../MunityCoverageReport
+rm -r MunityCoverageReport
 rm -r tests/MUNity.BlazorServer.Tests/TestResults
 rm -r tests/MUNityBaseTest/TestResults
 rm -r tests/MUNityDatabaseTest/TestResults
@@ -7,4 +7,4 @@ rm -r tests/MUNityServicesTest/TestResults
 
 dotnet test MUNityCore.sln --collect:"XPlat Code Coverage"
 dotnet tool install --global dotnet-reportgenerator-globaltool
-reportgenerator -reports:tests/**/coverage.cobertura.xml -targetdir:../MunityCoverageReport -reporttypes:Html
+reportgenerator -reports:tests/**/coverage.cobertura.xml -targetdir:MunityCoverageReport -reporttypes:Html
