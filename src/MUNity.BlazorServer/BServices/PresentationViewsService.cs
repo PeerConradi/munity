8fb7c980d7f3d267b8fb3a9aa8df3fe2cc8c871e
Aeuke Peer Conradi <aeuke-peer@t-online.de>
2022-05-03 20:28:05 +0200

    Banners can now be set inside the presentation views

---
+++ src/MUNity.BlazorServer/BServices/PresentationViewsService.cs
@@ -0,0 +1,42 @@
+﻿namespace MUNity.BlazorServer.BServices
+{
+    public class PresentationViewsService
+    {
+        private List<PresentationView> presentations = new List<PresentationView>();
+
+        public PresentationView GetPresentation(string committeeId)
+        {
+            var presentation = presentations.FirstOrDefault(x => x.CommitteeId == committeeId);
+            if (presentation == null)
+            {
+                presentation = new PresentationView()
+                {
+                    CommitteeId = committeeId,
+                };
+                presentations.Add(presentation);
+            }
+            return presentation;
+        }
+    }
+
+    public class PresentationView
+    {
+        public event EventHandler<string> ResolutionIdChanged;
+
+        public string CommitteeId { get; set; }
+
+        private string resolutionId;
+        public string ResolutionId
+        {
+            get => resolutionId;
+            set
+            {
+                if (resolutionId != value)
+                {
+                    resolutionId = value;
+                    ResolutionIdChanged?.Invoke(this, value);
+                }
+            }
+        }
+    }
+}
